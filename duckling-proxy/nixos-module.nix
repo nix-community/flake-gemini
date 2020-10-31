fd4d8a0985e05f726de6a0f7db9493b6d07dd59d
Emery Hemingway <ehmry@posteo.net>
2020-10-31 13:54:45 +0100

    duckling-proxy: init

---
+++ duckling-proxy/nixos-module.nix
@@ -0,0 +1,76 @@
+flake:
+{ config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.services.duckling-proxy;
+in {
+  options.services.duckling-proxy = {
+    enable = mkEnableOption "Gemini proxy to access the Web";
+
+    package = mkOption {
+      description = "Which package this service should use.";
+      type = types.package;
+      default = flake.packages.${config.nixpkgs.system}.duckling-proxy;
+    };
+
+    settings = mkOption {
+      description = ''
+        Command-line flags, see <link xlink:href="${pkgs.duckling-proxy.meta.homepage}">${pkgs.duckling-proxy.meta.homepage}</link>'';
+      type = with types; attrsOf (nullOr str);
+      example = {
+        citationMarkers = null;
+        citationStart = 3;
+      };
+      default = { };
+    };
+
+    address = mkOption {
+      type = types.str;
+      default = "127.0.0.1";
+      description = "Which address this service should listen on.";
+    };
+
+    port = mkOption {
+      type = types.port;
+      default = 1965;
+      description = "Which port this service should listen on.";
+    };
+
+    serverCert = mkOption {
+      type = types.str;
+      description = "Path to Gemini server TLS certificate.";
+    };
+
+    serverKey = mkOption {
+      type = types.str;
+      description = "Path to Gemini server TLS private key.";
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+
+    services.duckling-proxy.settings = {
+      inherit (cfg) serverCert serverKey;
+      address = cfg.address;
+      port = toString cfg.port;
+    };
+
+    systemd.services.duckling-proxy = {
+      description = "Gemini proxy to access the Web";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        DynamicUser = true;
+        ExecStart = let
+          flags = attrsets.mapAttrsToList (name: value:
+            if value == null then "--${name}" else "--${name} ${value}")
+            cfg.settings;
+        in "${cfg.package}/bin/duckling-proxy ${toString flags}";
+        Restart = "always";
+      };
+    };
+
+  };
+
+}
--- flake.nix
+++ flake.nix
@@ -13,11 +13,13 @@
           inherit (pkgs)
             amfora asuka av-98 castor kristall molly-brown ncgopher;
           inherit (pkgs.haskellPackages) diohsc;
+          duckling-proxy = pkgs.callPackage ./duckling-proxy { };
           gacme = pkgs.callPackage ./gacme { };
           lagrange = pkgs.callPackage ./lagrange { };
         });
 
       nixosModules = {
+        duckling-proxy = import ./duckling-proxy/nixos-module.nix self;
         molly-brown = import
           "${nixpkgs}/nixos/modules/services/web-servers/molly-brown.nix";
       };
